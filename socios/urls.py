c094a34e772d8b4c07e407e0c0d96eba055eb0c7
Anthony Rivas <antho.snsd@gmail.com>
2024-01-20 18:33:35 -0500

    "Añadido funciones de activar y desactivar socios, como la eliminación de socios"

--- socios/urls.py
+++ socios/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.hello_world),
     path('socios/', views.list_socios),
+    path('socios/all/', views.list_all_socios),
     path('socios/<int:pk>/', views.list_socio),
     path('socios/create/', views.create_socio),
     path('socios/<int:pk>/update/', views.update_socio),
-    path('socios/<int:pk>/delete/', views.delete_socio),
+    path('socios/<int:pk>/activate/', views.activate_socio),
+    path('socios/<int:pk>/deactivate/', views.deactivate_socio),
+    path('socios/<int:pk>/delete/', views.logic_delete_socio),
+    path('socios/<int:pk>/trueDelete/', views.delete_socio),
 ]
--- socios/views.py
+++ socios/views.py
@@ -7,9 +7,15 @@ from socios.models import Socio
 def hello_world(request):
     return Response({"message": "Hello, world!"})
 
+@api_view(['GET'])
+def list_all_socios(request):
+    socios = Socio.objects.all()
+    serializer = SocioSerializer(socios, many=True)
+    return Response(serializer.data)
+
 @api_view(['GET']) 
 def list_socios(request):
-    socios = Socio.objects.all()
+    socios = Socio.objects.exclude(estado="*")
     serializer = SocioSerializer(socios, many=True)
     return Response(serializer.data)
 
@@ -33,6 +39,27 @@ def update_socio(request, pk):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def activate_socio(request, pk):
+    socio = Socio.objects.get(id=pk)
+    socio.estado = "A"
+    socio.save()
+    return Response("Socio activado")
+
+@api_view(['PUT'])
+def deactivate_socio(request, pk):
+    socio = Socio.objects.get(id=pk)
+    socio.estado = "I"
+    socio.save()
+    return Response("Socio desactivado")
+
+@api_view(['PUT'])
+def logic_delete_socio(request, pk):
+    socio = Socio.objects.get(id=pk)
+    socio.estado = "*"
+    socio.save()
+    return Response("Socio eliminado logicamente")
 
 @api_view(['DELETE'])
 def delete_socio(request, pk):
